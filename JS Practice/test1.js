af57659f9780cb34d6f8bc2c6ca7303272f904a8
mukeshv1517 <151816293+mukeshv1517@users.noreply.github.com>
2023-11-25 10:23:04 +0530

    Add files via upload

---
+++ JS Practice/test1.js
@@ -0,0 +1,41 @@
+//Question no:1 name using variable & data types
+let Name="Mukesh";
+console.log(Name);
+//Question no:2 Adding two perameters by using function
+function addtwoNumbers(a,b)
+{ 
+    return a+b   
+}
+const a=10,b=30;
+ const sum=addtwoNumbers(a,b)
+ console.log(sum)
+ //Question no:3
+ function checkNumber(number)
+ {
+
+ }
+ // Question no:4
+ for (let i = 0; i < 6; i++) 
+ {
+    console.log(i);
+ }
+ //Question no:5 Arrays 
+ let color = ["red","green","blue"];
+ console.log(color);
+ 
+ //for(let i=0;i<9;i++)
+ //{
+   // console.log(i)
+ //}
+ //Question nno:6
+ let person={
+     Name:"king",
+     age:"30",
+     city:"Hyderabad",
+          }
+     console.log(person)
+    
+     
+     
+
+
